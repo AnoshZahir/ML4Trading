f222513e35617859b56d642284b95e75cb36ca1e
Anosh Zahir <anoshzahir@hotmail.com>
2017-09-10 14:12:24 +0200

    lesson7 upto calc/plot mean,std, & kurtosis.

--- 01/lesson7.py
+++ 01/lesson7.py
@@ -10,6 +10,7 @@ def compute_daily_returns(df):
     daily_returns.ix[0, :] = 0 # set daily returns for row 0 to 0
     return daily_returns
 
+#-----------test run------------------------------------------
 def test_run():
     #Read data
     dates = pd.date_range('2009-01-01', '2012-12-31')
@@ -21,5 +22,26 @@ def test_run():
     daily_returns = compute_daily_returns(df)
     plot_data(daily_returns, title = 'Daily returns', ylabel = 'Daily returns')
     
+    #Plot a histogram
+    daily_returns.hist(bins = 20)
+    
+    #Get mean and std dev
+    mean = daily_returns['SPY'].mean()
+    print('mean = ' + str(mean))
+    std_dev = daily_returns['SPY'].std()
+    print('std_dev = ' + str(std_dev))
+    
+    #show the mean value on the plot
+    plt.axvline(mean, color = 'w', linestyle = 'dashed', linewidth = 2)
+    
+    #show the std dev on theplot
+    plt.axvline(std_dev, color = 'r', linestyle = 'dashed', linewidth = 2)
+    plt.axvline(-std_dev, color = 'r', linestyle = 'dashed', linewidth = 2)
+    
+    #Compute kurtosis
+    print('kurtosis = ' + str(daily_returns.kurtosis()))
+
+    
+#-----------run------------------------------------------------
 if __name__ == '__main__':
     test_run()
